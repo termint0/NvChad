7bb711ad2e8ad0adf5a278f2114563880334de52
Prokop Viktorin <viktorin.prokop@gmail.com>
2024-04-17 23:27:50 +0200

    Hello there

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,23 +1,23 @@
--- EXAMPLE 
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+local base = require("nvchad.configs.lspconfig")
+local on_attach = base.on_attach
+local capabilities = base.capabilities
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
-end
-
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
+local lspconfig = require("lspconfig")
+-- lspconfig.ccls.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
+--
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
   capabilities = capabilities,
 }
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,107 @@
 require "nvchad.mappings"
+local M = {}
 
--- add yours here
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    }
+  }
+}
+M.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+        require('dap-python').test_method()
+      end
+    }
+  }
+}
 
-local map = vim.keymap.set
+M.general = {
+  t = {
+    ["<C-h>"] = {
+      function()
+        vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true)
+        vim.cmd("wincmd h")
+      end,
+      "Window left"
+    },
+    ["<C-j>"] = {
+      function()
+        vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true)
+        vim.cmd("wincmd j")
+      end,
+      "Window down"
+    },
+    ["<C-k>"] = {
+      function()
+        vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true)
+        vim.cmd("wincmd k")
+      end,
+      "Window up"
+    },
+    ["<C-l>"] = {
+      function()
+        vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true)
+        vim.cmd("wincmd l")
+      end,
+      "Window right"
+    },
+    ["<C-p>"] = {
+      "<Up> <CR>",
+      "Window right"
+    },
+  },
+  n = {
+    ["<leader>sh"] = {
+      function()
+        if vim.v.count > 0 then
+          vim.cmd("vert res -" .. vim.v.count)
+        else
+          vim.cmd("vert res -1")
+        end
+      end,
+      "Vertically narrow window"
+    },
+    ["<leader>sk"] = {
+      function()
+        if vim.v.count > 0 then
+          vim.cmd("res -" .. vim.v.count)
+        else
+          vim.cmd("res -1")
+        end
+      end,
+      "Vertically narrow window"
+    },
+    ["<leader>sj"] = {
+      function()
+        if vim.v.count > 0 then
+          vim.cmd("res +" .. vim.v.count)
+        else
+          vim.cmd("res +1")
+        end
+      end,
+      "Vertically narrow window"
+    },
+    ["<leader>sl"] = {
+      function()
+        if vim.v.count > 0 then
+          vim.cmd("vert res +" .. vim.v.count)
+        else
+          vim.cmd("vert res +1")
+        end
+      end,
+      "Vertically narrow window"
+    },
+  }
+}
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+return M
